2c28870bec087094461787580b27d0003552f08d
Yanuarsyah Fitrah Indra <ryanindraaa@gmail.com>
2021-05-25 19:30:07 +0800

    Tambah NoteAdapter

---
+++ app/src/main/java/id/ac/unhas/noteapp/NoteAdapter.kt
@@ -0,0 +1,48 @@
+package id.ac.unhas.noteapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter(private val context: Context?, private val listener: (Note, Int) -> Unit) :
+    RecyclerView.Adapter<NoteViewHolder>() {
+
+    private var notes = listOf<Note>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_note,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        if (context != null) {
+            holder.bindItem(context, notes[position], listener)
+        }
+    }
+
+    fun setNotes(notes: List<Note>) {
+        this.notes = notes
+        notifyDataSetChanged()
+    }
+}
+
+class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bindItem(context: Context, note: Note, listener: (Note, Int) -> Unit) {
+        itemView.noteTV.text = note.note
+
+        itemView.setOnClickListener {
+            listener(note, layoutPosition)
+        }
+    }
+
+}
