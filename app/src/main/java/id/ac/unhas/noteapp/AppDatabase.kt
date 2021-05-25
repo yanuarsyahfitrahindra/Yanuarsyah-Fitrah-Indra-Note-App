fa8866cd33d9d5af69fa02b46fceb862f648af48
Yanuarsyah Fitrah Indra <ryanindraaa@gmail.com>
2021-05-25 19:18:50 +0800

    Ganti File Database

--- app/src/main/java/id/ac/unhas/noteapp/AppDatabase.kt
+++ app/src/main/java/id/ac/unhas/noteapp/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ContactsContract.CommonDataKinds.Note::class], exportSchema = false, version = 1)
+@Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
