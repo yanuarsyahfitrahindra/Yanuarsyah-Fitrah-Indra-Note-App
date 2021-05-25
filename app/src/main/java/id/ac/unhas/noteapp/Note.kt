48e969545112587454ee9173e70c47a9a684b972
Yanuarsyah Fitrah Indra <ryanindraaa@gmail.com>
2021-05-25 18:42:10 +0800

    Tambah Entity

---
+++ app/src/main/java/id/ac/unhas/noteapp/Note.kt
@@ -0,0 +1,14 @@
+package id.ac.unhas.noteapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+    @ColumnInfo(name = "note")
+    var note: String
+)
